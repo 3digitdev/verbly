330ff768cfaae4973586f6f97b9ba1eea411ea7f
Max Bussiere <3digitdev@gmail.com>
2019-10-15 23:09:40 -0500

    more updates, styling, and a new endpoint/function

--- src/elm/Verbly.elm
+++ src/elm/Verbly.elm
@@ -20,32 +20,30 @@ main =
         }
 
 
-decodeVerb : JD.Decoder (List String)
-decodeVerb =
-    JD.list JD.string
-
-
-decodeAll : JD.Decoder (Dict String (List String))
-decodeAll =
+decodeObject : JD.Decoder ResponseObject
+decodeObject =
     JD.dict (JD.list JD.string)
 
 
 type alias Model =
     { currentConj : QueryResult
-    , searchVerb : String
+    , searchTerm : String
     , errors : String
     }
 
 
+type alias ResponseObject =
+    Dict String (List String)
+
+
 type QueryResult
-    = Single (List String)
-    | All (Dict String (List String))
+    = Resp ResponseObject
     | Empty
 
 
 initModel : Model
 initModel =
-    { searchVerb = ""
+    { searchTerm = ""
     , currentConj = Empty
     , errors = ""
     }
@@ -63,40 +61,51 @@ update msg model =
             ( model, Cmd.none )
 
         UpdateSearchTerm content ->
-            ( { model | searchVerb = content }, Cmd.none )
+            ( { model | searchTerm = content }, Cmd.none )
 
         GetConjugation ->
-            ( model, getConjugation model.searchVerb )
+            ( model, getConjugation model.searchTerm )
 
         GotAllConjugations result ->
             case result of
                 Err httpError ->
-                    ( { model | errors = errorToString httpError model.searchVerb, currentConj = Empty }, Cmd.none )
+                    ( { model | errors = errorToString httpError model.searchTerm, currentConj = Empty }, Cmd.none )
 
                 Ok conj ->
-                    ( { model | currentConj = All conj, errors = "" }, Cmd.none )
+                    ( { model | currentConj = Resp conj, errors = "" }, Cmd.none )
 
         GotConjugation result ->
             case result of
                 Err httpError ->
-                    ( { model | errors = errorToString httpError model.searchVerb, currentConj = Empty }, Cmd.none )
+                    ( { model | errors = errorToString httpError model.searchTerm, currentConj = Empty }, Cmd.none )
 
                 Ok conj ->
-                    ( { model | currentConj = Single conj, errors = "" }, Cmd.none )
+                    ( { model | currentConj = Resp conj, errors = "" }, Cmd.none )
+
+        GetVerbFromConjugation ->
+            ( model, getVerbFromConjugation model.searchTerm )
+
+        GotVerbFromConjugation result ->
+            case result of
+                Err httpError ->
+                    ( { model | errors = errorToString httpError model.searchTerm, currentConj = Empty }, Cmd.none )
+
+                Ok conj ->
+                    ( { model | currentConj = Resp conj, errors = "" }, Cmd.none )
 
 
-api : String -> String
-api verb =
+api : String -> String -> String
+api route data =
     let
         base =
-            "http://0.0.0.0:5000/verbs"
+            "http://0.0.0.0:5000" ++ route
     in
-    case verb of
+    case data of
         "" ->
             base
 
         default ->
-            base ++ "/" ++ verb
+            base ++ "/" ++ data
 
 
 getConjugation : String -> Cmd Msg
@@ -105,15 +114,28 @@ getConjugation verb =
         expect =
             case verb of
                 "" ->
-                    Http.expectJson GotAllConjugations decodeAll
+                    Http.expectJson GotAllConjugations decodeObject
 
                 default ->
-                    Http.expectJson GotConjugation decodeVerb
+                    Http.expectJson GotConjugation decodeObject
     in
     Http.get
-        { url = api verb
+        { url = api "/verbs" verb
         , expect = expect
         }
+
+
+getVerbFromConjugation : String -> Cmd Msg
+getVerbFromConjugation conjVerb =
+    case conjVerb of
+        "" ->
+            Cmd.none
+
+        default ->
+            Http.get
+                { url = api "/base" conjVerb
+                , expect = Http.expectJson GotVerbFromConjugation decodeObject
+                }
 
 
 errorToString : Http.Error -> String -> String
@@ -126,7 +148,7 @@ errorToString err verb =
             "Network error"
 
         Http.BadStatus resp ->
-            String.fromInt resp ++ ": Invalid verb '" ++ verb ++ "'"
+            String.fromInt resp ++ ": '" ++ verb ++ "' Not Found"
 
         Http.BadBody text ->
             "Unexpected response from api: " ++ text
@@ -140,9 +162,11 @@ errorToString err verb =
 
 
 type Msg
-    = GotAllConjugations (Result Http.Error (Dict String (List String)))
-    | GotConjugation (Result Http.Error (List String))
+    = GotAllConjugations (Result Http.Error ResponseObject)
+    | GotConjugation (Result Http.Error ResponseObject)
     | GetConjugation
+    | GetVerbFromConjugation
+    | GotVerbFromConjugation (Result Http.Error ResponseObject)
     | UpdateSearchTerm String
     | NoOp
 
@@ -160,45 +184,125 @@ subscriptions model =
 -- VIEW
 
 
+renderConjRow : String -> String -> Html Msg
+renderConjRow subj conj =
+    tr [] [ td [] [ text subj ], td [] [ text conj ] ]
+
+
 renderVerb : String -> List String -> Html Msg
-renderVerb verb conj =
-    div
-        []
-        [ strong [] [ text (verb ++ ": ") ]
-        , p [] [ text (String.join ", " conj) ]
+renderVerb verb conjList =
+    let
+        subjList =
+            [ "IO"
+            , "TU"
+            , "LEI/LUI"
+            , "NOI"
+            , "VOI"
+            , "LORO"
+            ]
+    in
+    li [ class "collection-item" ]
+        [ table
+            [ class "striped centered" ]
+            [ thead
+                []
+                [ tr []
+                    [ th [ colspan 2, class "center-align" ] [ h5 [] [ text (String.toUpper verb) ] ] ]
+                , tr []
+                    [ th [] [ text "Subject" ]
+                    , th [] [ text "Conjugation" ]
+                    ]
+                ]
+            , tbody
+                []
+                (List.map2 renderConjRow subjList conjList)
+            ]
         ]
 
 
-renderAll : List ( String, List String ) -> List (Html Msg)
-renderAll allVerbs =
-    List.map (\v -> renderVerb (first v) (second v)) allVerbs
-
-
-renderConjOutput : Model -> Html Msg
-renderConjOutput model =
+renderOutput : Model -> Html Msg
+renderOutput model =
     case model.currentConj of
-        All valDict ->
-            div
-                []
-                (renderAll (Dict.toList valDict))
-
-        Single val ->
-            renderVerb model.searchVerb val
+        Resp val ->
+            div []
+                [ ul
+                    [ class "collection" ]
+                    (List.map (\v -> renderVerb (first v) (second v)) (Dict.toList val))
+                ]
 
         Empty ->
             div [] []
+
+
+renderNavBar : Model -> Html Msg
+renderNavBar model =
+    nav []
+        [ div
+            [ class "nav-wrapper indigo" ]
+            [ a [ href "#", class "brand-logo center" ]
+                [ text "Verbly" ]
+            , ul
+                [ id "nav-mobile", class "left hide-on-med-and-down" ]
+                [ li [] [ a [ href "#" ] [ text "Conjugate" ] ]
+                , li [] [ a [ href "#" ] [ text "Translate" ] ]
+                ]
+            ]
+        ]
+
+
+renderSearchBar : Model -> Html Msg
+renderSearchBar model =
+    div
+        [ class "col s12" ]
+        [ div
+            [ class "row" ]
+            [ div
+                [ class "input-field col s4" ]
+                [ input
+                    [ placeholder "Enter Search Term"
+                    , Html.Attributes.id "search"
+                    , Html.Attributes.value model.searchTerm
+                    , onInput UpdateSearchTerm
+                    ]
+                    [ label
+                        [ for "search" ]
+                        [ text "Search:" ]
+                    ]
+                ]
+            ]
+        , div
+            [ class "row" ]
+            [ button
+                [ class "waves-effect waves-light btn-large orange darken-3 col s2"
+                , onClick GetConjugation
+                ]
+                [ text "Conjugate" ]
+            , button
+                [ class "waves-effect waves-light btn-large lime darken-2 col s2"
+                , onClick GetVerbFromConjugation
+                ]
+                [ text "Unconjugate" ]
+            ]
+        ]
 
 
 view : Model -> Html Msg
 view model =
     div
         []
-        [ input
-            [ onInput UpdateSearchTerm, Html.Attributes.value model.searchVerb ]
+        [ node "link"
+            [ rel "stylesheet"
+            , href "../../stylesheets/main.css"
+            ]
             []
-        , button
-            [ onClick GetConjugation ]
-            [ text "Conjugate" ]
-        , div [] [ text model.errors ]
-        , renderConjOutput model
+        , renderNavBar model
+        , div
+            [ class "container center-block" ]
+            [ div
+                [ class "center-block" ]
+                [ renderSearchBar model
+                , div [] [ text model.errors ]
+                , renderOutput model
+                ]
+            ]
         ]
--- src/nim/api.nim
+++ src/nim/api.nim
@@ -1,5 +1,6 @@
 import jester
 import json
+import strformat
 
 const VERBFILE = "../../data/verbs.json"
 
@@ -11,6 +12,15 @@ routes:
     get "/verbs/@verb":
         let jdata = parseFile(VERBFILE)
         if jdata.hasKey(@"verb"):
-            resp(Http200, {"Access-Control-Allow-Origin":"*"}, jdata[@"verb"].pretty)
+            var ret = newJObject()
+            ret.add(@"verb", jdata[@"verb"])
+            resp(Http200, {"Access-Control-Allow-Origin":"*"}, ret.pretty)
         else:
             resp(Http404, {"Access-Control-Allow-Origin":"*"}, (%*{}).pretty)
+
+    get "/base/@conj":
+        let jdata = parseFile(VERBFILE)
+        for k, v in jdata.pairs():
+            if v.contains(newJString(@"conj")):
+                resp(Http200, {"Access-Control-Allow-Origin":"*"}, (%*{ k: v }).pretty)
+        resp(Http404, {"Access-Control-Allow-Origin":"*"}, (%*{}).pretty)
